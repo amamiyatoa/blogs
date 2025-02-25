f4e8393dcabc5be0c803809b987507a277e7efc3
amamiyatoa <hrt.mr@icloud.com>
2025-02-25 15:55:46 +0900

    URLミスの修正

--- introductions/script.js
+++ introductions/script.js
@@ -17,7 +17,7 @@ window.onload = function() {
     document.getElementById('github_link').onclick = () => {
         let github_link = window.confirm('ページを移動してもよろしいですか？');
         if(github_link) {
-            window.open('https://github.com/amamiyatoa/blogs/introductions','_blank');
+            window.open('https://github.com/amamiyatoa/blogs/tree/master/introductions','_blank');
         }
     }
 }
